4d4b5685b7ddd3e70bacc3b98bfa89a69a9b2580
xyqyear <xyqyear@gmail.com>
2024-03-10 09:44:55 +0800

    board def for the second board

--- firmware/src/board_defs.h
+++ firmware/src/board_defs.h
@@ -10,11 +10,11 @@
 #define I2C_SCL 27
 #define I2C_FREQ 400*1000
 
-#define RGB_PIN 16
+#define RGB_PIN 17
 #define RGB_ORDER GRB // or RGB
-#define RGB_BUTTON_MAP { 4, 5, 6, 7, 0, 1, 2, 3, 8, 9, 10, 11 }
+#define RGB_BUTTON_MAP { 3, 2, 1, 0, 7, 6, 5, 4, 8, 9, 10, 11 }
 
-#define BUTTON_DEF { 15, 12, 13, 14, 8, 9, 10, 11, 12, 0, 1, 2}
+#define BUTTON_DEF { 7, 6, 5, 4, 3, 2, 1, 0, 8, 9, 10, 11}
 
 #define BUTTON_PULL_UP { true, true, true, true, true, true, true, true, true, true, true, true }
 // true means the button is active high, false means active low
@@ -25,9 +25,9 @@
 #define BUTTON_NKRO_MAP_P1 "\x1a\x08\x07\x06\x1b\x1d\x04\x14\x20\x3a\x3b\x3c"
 #define BUTTON_NKRO_MAP_P2 "\x60\x61\x5e\x5b\x5a\x59\x5c\x5f\x55\x3a\x3b\x3c"
 
-#define TOUCH_MAP { A8, E8, D8, B7, A7, E7, D7, B6, A6, E6, D6, A5, \
-                    B5, E5, C2, C1, D5, A4, B4, E4, D4, A3, B3, E3, \
-                    D3, A2, B2, E2, D2, B1, A1, E1, D1, B8 }
+#define TOUCH_MAP { A3, D3, E3, B2, A2, D2, E2, B1, A1, C1, D1, C2, \
+                    A6, D6, E6, B5, A5, D5, E5, B4, A4, D4, E4, B3, \
+                    E1, B8, A8, D8, E8, B7, A7, D7, E7, B6 }
 #else
 
 #endif
